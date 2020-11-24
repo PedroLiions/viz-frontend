c8a671ad531746c0d561c065c34beebe233c66a2
Pedro Lima <pedrolion1@hotmail.com>
2020-11-24 12:03:42 -0300

    Commit for test in server

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('', function () {
-    $query = "select
-    top(20)
-    *
-
-from
-    ftAnalyticsIndicadoresV2
-WHERE
-        Date >= '2020-11-11'
-  AND Date <= '2020-11-12'
-  AND CPCA IS NOT NULL
-  AND Mailing IS NOT NULL";
-
-    $result = \Illuminate\Support\Facades\DB::connection('sqlsrv')->select($query);
-
-    dd($result);
+Route::get('test-http', function () {
+    return 'PHP OK';
 });
